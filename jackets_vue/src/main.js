9d0d04346b8b12b7d69dde127951c8efbc6c943b
Eugenio <lugezz@gmail.com>
2022-07-16 10:27:03 -0300

    Connecting Vue to Django

--- jackets_vue/src/main.js
+++ jackets_vue/src/main.js
@@ -1,5 +1,9 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import store from './store'
+import axios from 'axios'
 
-createApp(App).use(router).mount('#app')
+axios.defaults.baseURL = 'http://localhost:8000'
+
+createApp(App).use(store).use(router, axios).mount('#app')
